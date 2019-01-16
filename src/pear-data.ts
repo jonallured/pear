8ec9e84864b736e17b1a577c1d83c6bd3916e802
Jon Allured <jon.allured@gmail.com>
2019-01-16 09:25:16 -0600

    Add command to list known authors

--- src/pear-data.ts
+++ src/pear-data.ts
@@ -1,9 +1,16 @@
+import {PearAuthor} from './pear-author'
 import {PearConfig} from './pear-config'
+import {noPearDataFileError} from './pear-errors'
 import {PearMessages} from './pear-messages'
 
 const fs = require('fs')
 
-const blankSlate = {current: [], known: []}
+interface PearDataFile {
+  current: PearAuthor[]
+  known: PearAuthor[]
+}
+
+const blankSlate: PearDataFile = {current: [], known: []}
 export const initialData = JSON.stringify(blankSlate)
 
 export class PearData {
@@ -15,12 +22,25 @@ export class PearData {
   }
 
   private readonly path: string
+  private _json?: PearDataFile
 
   constructor(path: string = PearConfig.dataPath()) {
     this.path = path
   }
 
-  get authors() {
-    return []
+  get json() {
+    if (this._json === undefined) this.loadJson()
+
+    return this._json
+  }
+
+  get known(): PearAuthor[] {
+    return this.json!.known || []
+  }
+
+  private loadJson = () => {
+    if (!fs.existsSync(this.path)) throw noPearDataFileError
+    const data = fs.readFileSync(this.path, {encoding: 'utf-8'})
+    this._json = JSON.parse(data)
   }
 }
