1f219d0ef9c24d29aeae05a19dc594eba88c1d40
Jon Allured <jon.allured@gmail.com>
2019-01-15 21:06:34 -0600

    Extract class for pear data
    This commit is where I move from the integration-type tests that you get
    from oclif to more unit-level tests because I just can't seem to get
    mocking to work with their testing tools. I've actually removed the
    generated test for init and replaced it with a jest spec.

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,25 +1,12 @@
 import {Command} from '@oclif/command'
 
-const fs = require('fs')
+import {PearData} from '../pear-data'
 
 export default class Init extends Command {
-  static description = 'create the ~/.pear-authors file'
+  static description = 'create the ~/.pear-data file'
 
   async run() {
-    // this command should:
-    // * install a git hook on post commit
-    // * create a ~/.pear-authors.json file
-
-    // check to see if the file exists
-    const path = '/Users/jon/.pear-authors.json'
-
-    if (!fs.existsSync(path)) {
-      const data = {authors: []}
-      const json = JSON.stringify(data)
-      fs.writeFileSync(path, json)
-    }
-    // create an empty one if not
-
-    this.log('Coauthors file created! 🍐')
+    const message = PearData.init()
+    this.log(message)
   }
 }
