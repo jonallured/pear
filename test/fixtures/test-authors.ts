4adc195c61f381ae81c10d80a2c7cd61374ccceb
Jon Allured <jon.allured@gmail.com>
2019-01-21 12:30:40 -0600

    Extract fixture data file

---
+++ test/fixtures/test-authors.ts
@@ -0,0 +1,4 @@
+import {PearAuthor} from '../../src/pear-author'
+
+export const erik: PearAuthor = {email: 'erik@example.com', name: 'Erik Krietsch', username: 'erik'}
+export const orta: PearAuthor = {email: 'orta@example.com', name: 'Orta Therox', username: 'orta'}
