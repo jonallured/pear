fe8f6d210bdbc82c5a7845cd6307cfa1e7b3de08
Jon Allured <jon.allured@gmail.com>
2019-01-21 13:30:12 -0600

    Add command for adding current author

--- src/pear-messages.ts
+++ src/pear-messages.ts
@@ -1,4 +1,5 @@
 export const PearMessages = {
+  addedCurrent: 'Added new current authors! 🍐',
   addedKnown: 'Added new known authors! 🍐',
   createdDataFile: 'Created data file! 🍐',
   foundDataFile: 'Found existing data file! 🍐'
