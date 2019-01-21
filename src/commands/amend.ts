0fbe012cb680dfd7f5d9a6e26fd227883a7563c3
Jon Allured <jon.allured@gmail.com>
2019-01-21 15:22:15 -0600

    Add command to amend previous commit

---
+++ src/commands/amend.ts
@@ -0,0 +1,45 @@
+import {Command} from '@oclif/command'
+
+import {PearData} from '../pear-data'
+import {PearError, trailersFoundError} from '../pear-errors'
+import {PearMessages} from '../pear-messages'
+import {PearUtils} from '../pear-utils'
+
+export default class Amend extends Command {
+  static description = 'amend last commit message with trailers'
+
+  async run() {
+    try {
+      const currentMessage = this.getCurrentMessage()
+      const newMessage = this.cleanMessage(currentMessage)
+      this.amendCommit(newMessage)
+      this.log(PearMessages.amendedCommit)
+    } catch (error) {
+      this.handleError(error)
+    }
+  }
+
+  private handleError(error: PearError) {
+    this.error(error.message, {exit: error.exit})
+  }
+
+  private getCurrentMessage(): string {
+    const logCommand = 'git log -1 --pretty=%B'
+    const message = PearUtils.exec(logCommand)
+    if (message.includes('Co-authored-by:')) throw trailersFoundError
+    return message
+  }
+
+  private cleanMessage(currentMessage: string): string {
+    const data = new PearData()
+    const trailers = data.trailer()
+
+    const cleanedMessage = currentMessage.replace(/Co-authored-by[\s\S]*/g, '')
+    return cleanedMessage + trailers
+  }
+
+  private amendCommit(newMessage: string) {
+    const amendCommand = `git commit --amend --allow-empty --message "${newMessage}"`
+    PearUtils.exec(amendCommand)
+  }
+}
